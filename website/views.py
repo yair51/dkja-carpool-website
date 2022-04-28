d8dabfb4db293617cc6832ed366903acaa87c107
Yair Gritzman <yairgritzman@gmail.com>
2022-04-28 13:05:43 -0400

    removed tkinter and threading imports

--- website/views.py
+++ website/views.py
@@ -1,5 +1,3 @@
-from threading import current_thread
-from tkinter.font import names
 from flask import Blueprint, render_template, request, flash, redirect, url_for
 from flask_login import login_required, current_user
 from flask_admin.contrib.sqla import ModelView
